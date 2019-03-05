7bd5cfe7bcd44e7730ad6ebbec0726472d257d77
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-05 20:31:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,20 +10,68 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var newItem = {itemName: item, itemPrice: Math.floor(Math.random() * 100)};
+  
+  if (cart.push(newItem)){
+  return `${item} has been added to your cart.`}
 }
 
 function viewCart() {
-  // write your code here
+  var newArray = [];
+if (cart.length === 0){
+  return "Your shopping cart is empty."
+} else if (cart.length === 1){
+  return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+} else if (cart.length === 2){
+  return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`
+} else if (cart.length > 2){
+  for (var i=0; i<cart.length; i++){
+    newArray.push(`${cart[i].itemName} at $${cart[i].itemPrice}`)
+  }
+  newArray[newArray.length - 1] = `and ${newArray[newArray.length - 1]}.`
+  return `In your cart, you have ${newArray.join(", ")}`
 }
+  
+}
+
+
 
 function total() {
-  // write your code here
+  var totalArray = []
+ for (let i = 0; i<cart.length; i++){
+   totalArray.push(cart[i].itemPrice)
+ }
+ const totalCart = totalArray.reduce((a, b) => {
+  return a + b
+ })
+ return totalCart
 }
 
+
+
 function removeFromCart(item) {
-  // write your code here
+  var cartContent = []; 
+  for (let i = 0; i < cart.length; i++){
+    cartContent.push(cart[i].itemName)
+  }
+  if (cartContent.includes(item) === false){
+    return "That item is not in your cart."
+  } else if (cartContent.includes(item) === true){
+    cart.splice(cart.indexOf(test)-1, 1)
+    return cart
+  }
+  
 }
+
+
+
+
+
+
+
+
+
+
 
 function placeOrder(cardNumber) {
   // write your code here
