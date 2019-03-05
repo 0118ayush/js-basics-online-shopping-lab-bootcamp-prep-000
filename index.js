c3f894872657909b7827240f8e514c3974e48783
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-03-05 21:47:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -71,9 +71,9 @@ function removeFromCart(item) {
 
 
 function placeOrder(cardNumber) {
-  if (cardNumber == undefined){
+  if (cardNumber === undefined){
     return "Sorry, we don't have a credit card on file for you."
-  }
+  } else if ()
 }
 
 
